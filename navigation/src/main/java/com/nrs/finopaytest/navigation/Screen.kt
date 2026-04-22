70f988c16d600194c4f7f96c622f8ce906b62887
darmilola <damilolaakinterinwa@gmail.com>
2026-04-22 20:13:27 +0100

    Add UI tests

--- navigation/src/main/java/com/nrs/finopaytest/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.nrs.finopaytest.navigation
-
-sealed class Screen(val route: String) {
-    object WeatherList : Screen("weather_list")
-    object WeatherDetail : Screen("weather_detail/{cityName}") {
-        fun createRoute(cityName: String) = "weather_detail/$cityName"
-    }
-}
